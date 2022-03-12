2127e0c363962272ac6461f9e1e0aee150fe8918
Matt Di Micelli <matthewdimicelli@gmail.com>
2022-03-11 22:16:59 -0500

    finish api

--- server/models/standup.js
+++ server/models/standup.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-const noEmptyFieldValidator = [input => input.trim().length > 0, 'Do not leave {PATH} empty'];
-const sizeValidator = [
-    input => input.trim().length > 0 && input.trim().length <50,
-    'Do not leave {PATH} empty'
-];
+const noEmptyFieldValidator = [input => (input.trim().length) > 0, 'Do not leave {PATH} empty'];
+// const noEmptyFieldValidator = [
+//     function (val) {
+//         let testVal = val.trim();
+//         return (testVal.length > 0);
+//     },
+//     'Please supply a value for {PATH}'
+// ]
 
 const standupSchema = new mongoose.Schema({
     _teamMemberId: { //when accessing this, you will need to query for _teamMemberId, since the value
@@ -12,11 +15,11 @@ const standupSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'teammembers',  // the name of the model to reference
     },  
-    teamMember: { type: String, required: true, validator: sizeValidator },
-    project: { type: String, required: true, validator: noEmptyFieldValidator },
-    workToday: { type: String, required: true, validator: noEmptyFieldValidator },
-    workYesterday: { type: String, required: true, validator: noEmptyFieldValidator },
-    impediment: { type: String, required: true, validator: noEmptyFieldValidator }, 
+    teamMember: { type: String, required: true, validate: noEmptyFieldValidator },
+    project: { type: String, required: true, validate: noEmptyFieldValidator },
+    workToday: { type: String, required: true, validate: noEmptyFieldValidator },
+    workYesterday: { type: String, required: true, validate: noEmptyFieldValidator },
+    impediment: { type: String, required: true }, 
     createdOn: { type: Date, default: Date.now },
 });
 
--- server/models/teamMember.js
+++ server/models/teamMember.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
+const sizeValidator = [
+    input => input.trim().length > 0 && input.trim().length <50,
+    '{PATH} must be greater than zero characters and less than 50 '
+];
+
 const teamMemberSchema = mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true, validate: sizeValidator },
 });
 
 module.exports = mongoose.model('TeamMember', teamMemberSchema, 'teammembers');
